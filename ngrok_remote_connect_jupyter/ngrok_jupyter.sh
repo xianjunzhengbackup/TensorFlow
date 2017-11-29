65484f9b23510210309ed3c109c8fee56614e1df
xianjunzhengbackup <xianjunzheng.backup@gmail.com>
2017-11-29 15:46:14 +1100

    get ngrok_jupyter.sh work

--- ngrok_remote_connect_jupyter/ngrok_jupyter.sh
+++ ngrok_remote_connect_jupyter/ngrok_jupyter.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-#txt=$(/home/jun/anaconda3/bin/jupyter-notebook)
-#echo $txt
-setsid ngrok http 8888 &
-sleep 5s
+/usr/bin/setsid /home/jun/anaconda3/bin/jupyter notebook --no-browser &
+sleep 20s
+txt=$(/home/jun/anaconda3/bin/jupyter notebook list)
+token=$(echo $txt | grep -o "token=[0-9a-z]*")
+/usr/bin/setsid /home/jun/ngrok http 8888 &
+sleep 20s
 txt=$(GET http://localhost:4040/status)
 link=$(echo $txt | grep -o "https://[0-9a-z]*\.ngrok\.io")
-echo $link
+echo $link > /home/jun/ngrok_jupyter_email.txt
+echo $token >>/home/jun/ngrok_jupyter_email.txt
+sleep 20s
+cat /home/jun/ngrok_jupyter_email.txt | mutt -s "ngrok jupyter login remote info" wolff_zheng@hotmail.com
