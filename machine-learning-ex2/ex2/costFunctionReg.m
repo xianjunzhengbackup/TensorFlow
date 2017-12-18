687e2470fa915e0fc77686d02c4b61634c9e0f04
xianjunzhengbackup <xianjunzheng.backup@gmail.com>
2017-12-18 14:59:52 +1100

    add week3'assignment for andrew ng

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,7 +17,11 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
-
+h_theta = sigmoid(X * theta);
+J= 1 / m * sum(-y.*log(h_theta)-(1-y).*log(1-h_theta)) + lambda / (2 * m) *(sum(theta .^2) - theta(1,1)^2);
+theta_j=theta;
+theta_j(1,1)=0;
+grad = 1/m*((h_theta -y)' * X)' + lambda / m * theta_j;
 
 
 
--- machine-learning-ex2/ex2/predict.m
+++ machine-learning-ex2/ex2/predict.m
@@ -14,7 +14,8 @@ p = zeros(m, 1);
 %               your learned logistic regression parameters. 
 %               You should set p to a vector of 0's and 1's
 %
-
+p=sigmoid(X * theta);
+p=(p>=0.5);
 
 
 
