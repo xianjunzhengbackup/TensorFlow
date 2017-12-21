eb21e2169b72030a9ee30f4d317050db427bf8d3
Xianjun Zheng <u5202104@uds.anu.edu.au>
2017-12-22 09:01:24 +1100

    modify assignment3

--- machine-learning-ex3/machine-learning-ex3/ex3/predictOneVsAll.m
+++ machine-learning-ex3/machine-learning-ex3/ex3/predictOneVsAll.m
@@ -28,8 +28,10 @@ X = [ones(m, 1) X];
 %       max element, for more information see 'help max'. If your examples 
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
-%       
+%  
 
+tmp = X * all_theta';
+[Y p] = max(tmp,[],2);
 
 
 
