12cb00a91fe7d59c6b01fe30ae49cf921ba752b6
xianjunzhengbackup <xianjunzheng.backup@gmail.com>
2018-01-16 13:49:11 +1100

    finish assignment?

--- machine-learning-ex8/machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -39,8 +39,9 @@ Theta_grad = zeros(size(Theta));
 %        Theta_grad - num_users x num_features matrix, containing the 
 %                     partial derivatives w.r.t. to each element of Theta
 %
-J = sum(sum(((X * Theta' - Y) .* R).^2)) / 2;
-X_grad = ((X * Theta' - Y) .* R) * Theta ;
+J = sum(sum(((X * Theta' - Y) .* R).^2)) / 2 + lambda / 2 * sum(sum(Theta .^ 2)) + lambda /2 * sum(sum(X .^2));
+X_grad = ((X * Theta' - Y) .* R) * Theta + lambda * X;
+Theta_grad = ((X * Theta' - Y) .* R)' * X + lambda * Theta;
 
 
 
